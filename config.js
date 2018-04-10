76d4e73c06175871777b4818174e9e092dc9de76
wnete <38018218+wnete@users.noreply.github.com>
2018-04-10 13:31:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
- 'TelegramProductionURL': 'https://t.me/wnete', // do not forget trailing "/" !!
+ 'TelegramProductionURL': 'https://t.me/wnetebot', // do not forget trailing "/" !!
  'TelegramToken': '575129053:AAEDPltJ_SjfG4xP4a6-ktjKFUQcT6KWSU0', // Telegram token
  'googleSheetKey': '1QDGoz7TuNk0r5yIB1U2OOwYWN3NMEtlvxT-p3kgNyzA', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
  "confTimeZone": "America/Bogota" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
